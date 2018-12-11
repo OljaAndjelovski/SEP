7b0c6a41ab0e54e4129f2214bdba8f8918442df3
Olja Andjelovski <andjelovskiolja@gmail.com>
2018-12-11 16:49:05 +0100

    Error solved

--- payment_gateway/src/main/java/com/ftn/uns/payment_gateway/repository/BankRepository.java
+++ payment_gateway/src/main/java/com/ftn/uns/payment_gateway/repository/BankRepository.java
@@ -2,11 +2,13 @@ package com.ftn.uns.payment_gateway.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.ftn.uns.payment_gateway.model.Bank;
 
+
 public interface BankRepository extends JpaRepository<Bank,Integer>{
 
-	@Query("select b from Bank where merchantId = :merchantId")
+	//@Query("select b from Bank where merchantId = :merchantId")
 	public Bank findByMerchantId(String merchantId);
 }
