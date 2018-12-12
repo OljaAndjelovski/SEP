0b1125524d9ed232d8a759ecf46fbaf054529191
Valentina Kucenic <vkucenic@gmail.com>
2018-12-12 08:14:17 +0100

    isprvljena greska

--- payment_gateway/src/main/java/com/ftn/uns/payment_gateway/repository/BankRepository.java
+++ payment_gateway/src/main/java/com/ftn/uns/payment_gateway/repository/BankRepository.java
@@ -2,13 +2,11 @@ package com.ftn.uns.payment_gateway.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.ftn.uns.payment_gateway.model.Bank;
 
+public interface BankRepository extends JpaRepository<Bank, Integer> {
 
-public interface BankRepository extends JpaRepository<Bank,Integer>{
-
-	//@Query("select b from Bank where merchantId = :merchantId")
+	@Query("select b from Bank b where b.merchantId = :merchantId")
 	public Bank findByMerchantId(String merchantId);
 }
